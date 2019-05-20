738f696c083b784ef3cca944f95069f7470adeff
André Spadetto Leão <andre.sp.leao@gmail.com>
2019-05-20 18:23:06 -0300

    Adding base classes to work with fragments and network calls.

---
+++ app/src/main/java/br/com/bakingapp/data/Constants.java
@@ -0,0 +1,10 @@
+package br.com.bakingapp.data;
+
+public class Constants {
+    static final String SCHEME = "https";
+    static final String HOST = "d17h27t6h515a5.cloudfront.net";
+    public static final int NETWORK_ERROR_CODE = 503;
+    public static final int GENERIC_ERROR_CODE = 500;
+
+
+}
---
+++ app/src/main/java/br/com/bakingapp/data/ServiceGenerator.java
@@ -0,0 +1,58 @@
+package br.com.bakingapp.data;
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.squareup.moshi.Moshi;
+
+import br.com.bakingapp.BuildConfig;
+import br.com.bakingapp.utils.BigDecimalAdapter;
+import br.com.bakingapp.utils.DateAdapter;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+import static android.os.Build.HOST;
+import static android.service.notification.Condition.SCHEME;
+
+public class ServiceGenerator {
+
+
+    private static final Moshi moshiFactory = new Moshi.Builder()
+            .add(new BigDecimalAdapter())
+            .add(new DateAdapter())
+            .build();
+
+    private static final Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(buildUrl())
+                    .addConverterFactory(MoshiConverterFactory.create(moshiFactory));
+
+    private static Retrofit retrofit = builder.build();
+
+    private static final HttpLoggingInterceptor logging =
+            new HttpLoggingInterceptor()
+                    .setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    private static final OkHttpClient.Builder httpClient =
+            new OkHttpClient.Builder();
+
+    public static <S> S createService(
+            Class<S> serviceClass) {
+        if (!httpClient.interceptors().contains(logging) && BuildConfig.DEBUG) {
+            httpClient.addNetworkInterceptor(new StethoInterceptor());
+            httpClient.addInterceptor(logging);
+            builder.client(httpClient.build());
+            retrofit = builder.build();
+        }
+
+        return retrofit.create(serviceClass);
+    }
+
+    private static HttpUrl buildUrl() {
+        return new HttpUrl.Builder()
+                .scheme(SCHEME)
+                .host(HOST)
+                .build();
+    }
+}
