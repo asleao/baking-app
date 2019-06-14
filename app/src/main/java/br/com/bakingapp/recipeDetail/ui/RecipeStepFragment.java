a03b870e8b159c755b587287a2026200cee826d3
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-06-13 21:14:48 -0300

    Exoplayer playing step.

--- app/src/main/java/br/com/bakingapp/recipeDetail/ui/RecipeStepFragment.java
+++ app/src/main/java/br/com/bakingapp/recipeDetail/ui/RecipeStepFragment.java
@@ -1,39 +1,42 @@
 package br.com.bakingapp.recipeDetail.ui;
 
-import androidx.lifecycle.ViewModelProviders;
-
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
 import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.IOException;
+import java.util.EventListener;
 
 import br.com.bakingapp.R;
 import br.com.bakingapp.recipeDetail.viewModel.RecipeStepViewModel;
 import br.com.bakingapp.services.recipeService.response.Step;
 
-public class RecipeStepFragment extends Fragment {
+public class RecipeStepFragment extends Fragment implements ExtractorMediaSource.EventListener {
 
     private RecipeStepViewModel mViewModel;
     private Step step;
     private PlayerView mPlayerView;
-    private SimpleExoPlayer player;
+    private SimpleExoPlayer mPlayer;
+
 
     public static RecipeStepFragment newInstance() {
         return new RecipeStepFragment();
@@ -50,6 +53,23 @@ public class RecipeStepFragment extends Fragment {
 
     private void setupFields(View view) {
         mPlayerView = view.findViewById(R.id.pv_recipe_step);
+        initializePlayer(Uri.parse(step.getVideoURL()));
+    }
+
+    private void initializePlayer(Uri mediaUrl) {
+        if (mPlayer == null) {
+            mPlayer = ExoPlayerFactory.newSimpleInstance(requireContext(), new DefaultTrackSelector(), new DefaultLoadControl());
+            mPlayerView.setPlayer(mPlayer);
+            String user = Util.getUserAgent(requireContext(), getResources().getString(R.string.app_name));
+            DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(requireContext(), user);
+            DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUrl, dataSourceFactory,
+                    extractorsFactory,
+                    new Handler(),
+                    this);
+            mPlayer.prepare(mediaSource);
+            mPlayer.setPlayWhenReady(true);
+        }
     }
 
     @Override
@@ -63,5 +83,10 @@ public class RecipeStepFragment extends Fragment {
             RecipeStepFragmentArgs args = RecipeStepFragmentArgs.fromBundle(getArguments());
             step = args.getStep();
         }
+    }
+
+    @Override
+    public void onLoadError(IOException error) {
+
     }
 }
